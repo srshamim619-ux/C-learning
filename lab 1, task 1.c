ed283fbd35efb05f256fa11c4801dc8698fa5f30
srshamim619-ux <srshamim619@gmail.com>
2026-09-16 19:06:31 +0600

    Add files via upload

---
+++ lab 1, task 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+ printf("Name: MD.SHAMIM REZA\n");
+ printf("Student ID:2508169\n");
+ printf("Level-2 , Semester-1\n");
+ printf("Depertment: PHYSICS\n");
+ return 0;
+ }
